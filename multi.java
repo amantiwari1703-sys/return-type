3b2c8c794b704030531d272024d05cf7882def47
Aman Tiwari <amantiwari@gmail.com>
2026-04-09 18:24:12 +0530

    no return

---
+++ multi.java
@@ -0,0 +1,18 @@
+public class multi {
+
+
+    public static String product(String a, String b) {
+        String name =" aditya ,aman";
+        return name;
+    }
+
+    public static void main(String[] args) {
+//        int a = add(23,1);
+//        System.out.println(a);
+        String of=product("raja","rani");
+        System.out.println(of);
+
+    }
+   }
+
+
