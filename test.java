d4b41c9bc3adc6733e867d42e7d0c3382de4865e
Aman Tiwari <amantiwari@gmail.com>
2026-04-10 19:56:30 +0530

    greet

--- test.java
+++ test.java
@@ -1,15 +1,18 @@
 public class test {
-  int max(int a,int b) {
-      int result;
-      if (a > b)
-          result = a;
-      else
-          result = b;
-      return result;
+    String greet(){
+        String msg="good morning";
+        return msg;
+//  int max(int a,int b) {
+//      int result;
+//      if (a > b)
+//          result = a;
+//      else
+//          result = b;
+//      return result;
   }
   public static void main(String[] args){
       test t=new test();
-      System.out.println(t.max(10,20));
+      System.out.println(t.greet());
 
 
     }
