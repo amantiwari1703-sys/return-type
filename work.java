96e8e406d65095c073f635ffd110c654243d585d
Aman Tiwari <amantiwari@gmail.com>
2026-04-10 09:56:47 +0530

    access

--- work.java
+++ work.java
@@ -1,14 +1,26 @@
 class work {
-    private static int age=23;
-    public static void main (String[] args){
-        System.out.println(age);
-        bajpai b=new bajpai();
-        System.out.println(b.num);
-
+    public static String name="arora";
+    public static void main(String args[]) {
+        System.out.println(name);
+     pankaj p=new pankaj();
+        System.out.println(p.books);
 
     }
 }
-class bajpai{
-    public static int num=24;
+//    private static int age=23;
+//    public static void main (String[] args){
+//        System.out.println(age);
+//        bajpai b=new bajpai();
+//        System.out.println(b.num);
 
+//
+//
+//    }
+//}
+    class pankaj{
+    public static String books="Bhagwat Gita";
 }
+//class bajpai{
+//    public static int num=24;
+//
+//}
