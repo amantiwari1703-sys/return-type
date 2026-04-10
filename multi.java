e9ddeb4a4ff509b6b9e32f565d85f8c0b738afe2
Aman Tiwari <amantiwari@gmail.com>
2026-04-10 09:46:25 +0530

    access modifier

--- multi.java
+++ multi.java
@@ -1,16 +1,17 @@
 public class multi {
 
 
-    public static float product(int a, int b) {
-        float multiply =a*b ;
+    public static int product(int a, int b) {
+        int multiply =a*b ;
         return multiply;
     }
 
     public static void main(String[] args) {
 //        int a = add(23,1);
 //        System.out.println(a);
-        float of=product(25,15);
+        int of=product(25,15);
         System.out.println(of);
+
 
 
 
---
+++ work.java
@@ -0,0 +1,14 @@
+class work {
+    private static int age=23;
+    public static void main (String[] args){
+        System.out.println(age);
+        bajpai b=new bajpai();
+        System.out.println(b.num);
+
+
+    }
+}
+class bajpai{
+    public static int num=24;
+
+}
