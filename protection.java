d4571c5e3af3e6f005bcfbccea1d1e180b38dea1
Aman Tiwari <amantiwari@gmail.com>
2026-04-10 19:40:49 +0530

    double

---
+++ protection.java
@@ -0,0 +1,18 @@
+public class protection {
+    protected String name="aman";
+    protected String books="chemistry";
+    protected int  sum=34;
+}
+class sanjay extends protection{
+private int num=24;
+public static void main(String args[]){
+    sanjay s=new sanjay();
+    System.out.println(s.num);
+
+
+
+
+}
+
+
+}
---
+++ test.java
@@ -0,0 +1,18 @@
+public class test {
+    double divide(double a,double b){
+        double divide=a/b;
+
+//    String fullName(String first,String last){
+//        String name=first+""+last;
+       return divide;
+
+
+
+    }
+    public static void main(String args[]){
+        test t=new test();
+        System.out.println(t.divide(33,33));
+
+    }
+
+}
