377ee9780a7d6ebe785fb91410cddf2550861ae8
Aman Tiwari <amantiwari@gmail.com>
2026-04-11 09:35:07 +0530

    interest

--- test.java
+++ test.java
@@ -1,16 +1,22 @@
 public class test {
-    int length(String s){
-        int len=s.length();
-        return len;
+    float interest(float p, float r, float t) {
+        float si = (p * r * t) / 100;
+        return si;
 
-//    String check(int n){
+
+    }
+
+//        int len=s.length();
+//        return len;
+//
+//   String check(int n){
 //        String result;
 //        if(n%2==0)
-//            result="even";
+//           result="even";
 //            else
 //                result="odd";
 //            return result;
-
+//
 //    int check(int a){
 //        int result;
 //        if(a%2==0)
@@ -18,25 +24,27 @@ public class test {
 //         else
 //             result=0;
 //         return result;
-
-
+//
+//
 //        return result;
 //        String msg="good morning";
-//        return ;
-//  int max(int a,int b) {
+//       return ;
+// int max(int a,int b) {
 //      int result;
-//      if (a > b)
-//          result = a;
-//      else
-//          result = b;
-//      return result;
-  }
-  public static void main(String[] args){
-      test t=new test();
-      System.out.println(t.length("aman"));
+//     if (a > b)
+//         result = a;
+//     else
+//         result = b;
+//     return result;
 
-
+    public static void main(String[] args) {
+        test t = new test();
+        System.out.println(t.interest(3, 4, 5));
 
     }
-
 }
+
+
+
+
+
