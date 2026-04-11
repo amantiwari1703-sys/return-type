e75ae747ef784299f3cf5e57312f20d56a156aa0
Aman Tiwari <amantiwari@gmail.com>
2026-04-11 09:48:21 +0530

    temperature

--- test.java
+++ test.java
@@ -1,45 +1,15 @@
 public class test {
-    float interest(float p, float r, float t) {
-        float si = (p * r * t) / 100;
-        return si;
+    double convert(double c){
+        double f=(c*9/5)+32;
+        return f;
+
 
 
     }
 
-//        int len=s.length();
-//        return len;
-//
-//   String check(int n){
-//        String result;
-//        if(n%2==0)
-//           result="even";
-//            else
-//                result="odd";
-//            return result;
-//
-//    int check(int a){
-//        int result;
-//        if(a%2==0)
-//            result=1;
-//         else
-//             result=0;
-//         return result;
-//
-//
-//        return result;
-//        String msg="good morning";
-//       return ;
-// int max(int a,int b) {
-//      int result;
-//     if (a > b)
-//         result = a;
-//     else
-//         result = b;
-//     return result;
-
     public static void main(String[] args) {
         test t = new test();
-        System.out.println(t.interest(3, 4, 5));
+        System.out.println(t.convert(4));
 
     }
 }
